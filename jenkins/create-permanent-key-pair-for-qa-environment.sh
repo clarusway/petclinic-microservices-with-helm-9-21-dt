f768435e0cbdd16d4dcc98669fb07fd7481ac6aa
james-clarusway <james@clarusway.com>
2022-03-30 08:30:37 +0000

    30mar1130

--- jenkins/create-permanent-key-pair-for-qa-environment.sh
+++ jenkins/create-permanent-key-pair-for-qa-environment.sh
@@ -1,9 +1,9 @@
 PATH="$PATH:/usr/local/bin"
 APP_NAME="petclinic"
-CFN_KEYPAIR="matt-${APP_NAME}-qa.key"
+ANS_KEYPAIR="matt-${APP_NAME}-qa.key"
 AWS_REGION="us-east-1"
-aws ec2 create-key-pair --region ${AWS_REGION} --key-name ${CFN_KEYPAIR} --query "KeyMaterial" --output text > ${CFN_KEYPAIR}
-chmod 400 ${CFN_KEYPAIR}
+aws ec2 create-key-pair --region ${AWS_REGION} --key-name ${ANS_KEYPAIR} --query "KeyMaterial" --output text > ${ANS_KEYPAIR}
+chmod 400 ${ANS_KEYPAIR}
 mkdir -p ${JENKINS_HOME}/.ssh
-mv ${CFN_KEYPAIR} ${JENKINS_HOME}/.ssh/${CFN_KEYPAIR}
+mv ${ANS_KEYPAIR} ${JENKINS_HOME}/.ssh/${ANS_KEYPAIR}
 ls -al ${JENKINS_HOME}/.ssh
