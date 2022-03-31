71761fff21c308f8844deee9dd82e5de2fdb0fc9
james-clarusway <james@clarusway.com>
2022-03-31 06:40:41 +0000

    09:40

--- ansible/scripts/deploy_app_on_qa_environment.sh
+++ ansible/scripts/deploy_app_on_qa_environment.sh
@@ -1,5 +1,9 @@
-PATH="$PATH:/usr/local/bin"
-APP_NAME="petclinic"
-sed -i "s/APP_STACK_NAME/${APP_STACK_NAME}/" ./ansible/inventory/qa_stack_dynamic_inventory_aws_ec2.yaml
-envsubst < docker-compose-swarm-qa.yml > docker-compose-swarm-qa-tagged.yml
-ansible-playbook -i ./ansible/inventory/qa_stack_dynamic_inventory_aws_ec2.yaml -b --extra-vars "workspace=${WORKSPACE} app_name=${APP_NAME} aws_region=${AWS_REGION} ecr_registry=${ECR_REGISTRY}" ./ansible/playbooks/pb_deploy_app_on_qa_environment.yaml
+echo 'Deploying App on Kubernetes'
+sh "envsubst < k8s/petclinic_chart/values-template.yaml > k8s/petclinic_chart/values.yaml"
+sh "sed -i s/HELM_VERSION/${BUILD_NUMBER}/ k8s/petclinic_chart/Chartyaml"
+sh "helm repo add stable-petclinic s3://petclinic-helm-charts/stablemyapp/"
+sh "helm package k8s/petclinic_chart"
+sh "helm s3 push petclinic_chart-${BUILD_NUMBER}.tgz stable-petclinic"
+sh "envsubst < ansible/playbooks/qa-petclinic-deploy-template >ansible/playbooks/qa-petclinic-deploy.yaml"
+sh "sleep 60"    
+sh "ansible-playbook -i ./ansible/inventory/qa_stack_dynamic_inventory_aws_ec2.yaml ./ansible/playbooks/qa-petclinic-deploy.yaml"
